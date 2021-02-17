8c811ab1a61368c378a98e27520475fbeaf19c3d
S Akash Janardhan <sakash.janardhan@citrix.com>
2021-02-18 01:16:41 +0530

    Trying to read from properties file present within jenkins workspace

--- JenkinsTestFolder/hello.java
+++ JenkinsTestFolder/hello.java
@@ -1,8 +1,27 @@
 package JenkinsTestFolder;
+import java.util.*;
+import java.io.*;
 
 public class hello {
     public static void main(String args[]){
         System.out.println("Hello from Java");
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        final Properties p = getProperties();
+        System.out.println("subdomain = " + p.getProperty("subdomain"));
+        
+    }
+
+    private static Properties getProperties(){
+
+        try {
+            FileReader reader=new FileReader(System.getProperty("user.dir") + "JenkinsTestFolder/a.properties");
+            Properties p=new Properties();
+            p.load(reader);
+            return p;
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
