8426576b68dd6f1569d9fe82d2e7027ad3a8e7a4
S Akash Janardhan <sakash.janardhan@citrix.com>
2021-02-18 01:05:26 +0530

    printing current working directory

--- JenkinsTestFolder/hello.java
+++ JenkinsTestFolder/hello.java
@@ -3,5 +3,6 @@ package JenkinsTestFolder;
 public class hello {
     public static void main(String args[]){
         System.out.println("Hello from Java");
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
     }
 }
