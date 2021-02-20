89a0128b14e737b7ca13603ee2047331c4b2c39a
S Akash Janardhan <sakash.janardhan@citrix.com>
2021-02-20 19:40:01 +0530

    added code to test app install from playstore

--- testApp/app/src/androidTest/java/com/example/jenkinstestapp/ExampleTest.java
+++ testApp/app/src/androidTest/java/com/example/jenkinstestapp/ExampleTest.java
@@ -2,11 +2,13 @@ package com.example.jenkinstestapp;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.BySelector;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.Until;
 
@@ -30,6 +32,37 @@ public class ExampleTest {
     public void startApp(){
         openApp(mDevice,"com.android.vending");
         mDevice.pressBack();
+        //added code to test app install from playstore
+        install("com.zenprise");
+    }
+
+    public void install(String packageId){
+        openPlayStorePageOfApp(packageId);
+        waitForTextAndClick("Install",10000);
+        assertTrue(waitForInstallSuccess());
+        mDevice.pressBack();
+    }
+
+    private boolean waitForInstallSuccess(){
+        return isObjectVisible(mDevice, By.text("Uninstall"),40000);
+    }
+
+    private void openPlayStorePageOfApp(String packageId){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://play.google.com/store/apps/details?id=" + packageId));
+        intent.setPackage("com.android.vending");
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Context context = ApplicationProvider.getApplicationContext();
+        context.startActivity(intent);
+    }
+
+    private boolean isObjectVisible(UiDevice mDevice, BySelector object, long timeout){
+        return mDevice.wait(Until.hasObject(object),timeout);
+    }
+
+    private void waitForTextAndClick(String text,long timeout){
+        isObjectVisible(mDevice,By.textContains(text),timeout);
+        mDevice.findObject(By.textContains(text)).click();
     }
 
     private static void openApp(UiDevice mDevice,String packageName){
